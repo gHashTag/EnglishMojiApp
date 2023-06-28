dc33638985cdc0ca8e5bdd79c47b66034d587db0
Dmitriy Vasilev <raoffonom@icloud.com>
2023-06-28 21:24:13 +0700

    splash screen ios

--- ios/EnglishMojiApp/AppDelegate.mm
+++ ios/EnglishMojiApp/AppDelegate.mm
@@ -1,7 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
-#import "RNSplashScreen.h" 
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -10,7 +10,7 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  [RNSplashScreen show];  
+  
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
